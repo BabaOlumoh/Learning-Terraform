4b6ca0a67f020c3a1417fe5dab36bdfc6a202dea
BabaOlumoh <olumohbabatunde@gmail.com>
2025-10-21 07:45:58 +0100

    Naming Convention

--- Azure/Lab2/main.tf
+++ Azure/Lab2/main.tf
@@ -1,5 +1,5 @@
 
 resource "azurerm_resource_group" "main" {
-  name = "rg-foobar"
-  location = "westus3"
+  name = "rg-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
 }
