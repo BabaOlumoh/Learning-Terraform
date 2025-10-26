96758c75a1b4a9bf57b6681caa5aed5a76577dad
BabaOlumoh <olumohbabatunde@gmail.com>
2025-10-26 11:24:03 +0000

    Practising moving tfstate to backend

---
+++ Practice Lab/Storage-Container-tfstate/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.49.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = " ~>3.6.3"
+    }
+  }
+  backend "azurerm" {
+    
+  }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+}
