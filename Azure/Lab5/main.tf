82aa6a8fd8b45186577ff1f88596766dbfb7c669
BabaOlumoh <olumohbabatunde@gmail.com>
2025-10-27 20:47:35 +0000

    IP Lookup

--- Azure/Lab5/main.tf
+++ Azure/Lab5/main.tf
@@ -60,11 +60,15 @@ resource "azurerm_network_security_group" "remote_access" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = chomp(data.http.my_ip.body)
     destination_address_prefix = "*"
   }
 }
 resource "azurerm_subnet_network_security_group_association" "alpha_remote_access" {
   subnet_id                 = azurerm_subnet.alpha.id
   network_security_group_id = azurerm_network_security_group.remote_access.id
+}
+
+data "http" "my_ip"{
+  url = "https://ifconfig.me/ip"
 }
