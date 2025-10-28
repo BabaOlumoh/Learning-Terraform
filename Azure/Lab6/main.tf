280fa37337e6c399d5f9f3af2f7170a3de9c199b
BabaOlumoh <olumohbabatunde@gmail.com>
2025-10-28 08:03:02 +0000

    Storing SSH in Key Vaults

--- Azure/Lab6/main.tf
+++ Azure/Lab6/main.tf
@@ -34,17 +34,24 @@ resource "tls_private_key" "vm1" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
-  content = tls_private_key.vm1.private_key_pem
-  filename = pathexpand("~/.ssh/vm1")
-  file_permission = "0600"
+
+data "azurerm_key_vault" "main" {
+  name                = "kv-devops-dev-284634"
+  resource_group_name = "rg-devops-dev"
 }
 
-resource "local_file" "public_key" {
-  content = tls_private_key.vm1.public_key_openssh
-  filename = pathexpand("~/.ssh/vm1.pub")
-  
+resource "azurerm_key_vault_secret" "vm_ssh_private" {
+  name         = "vm1-ssh-private"
+  value        = tls_private_key.vm1.private_key_pem
+  key_vault_id = data.azurerm_key_vault.main.id
 }
+
+resource "azurerm_key_vault_secret" "vm_ssh_public" {
+  name         = "vm1-ssh-public"
+  value        = tls_private_key.vm1.public_key_openssh
+  key_vault_id = data.azurerm_key_vault.main.id
+}
+
 
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                = "vm1${var.application_name}${var.environment_name}"
