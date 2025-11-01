17703eeeeaee0959c0200f30e71e921673e87817
BabaOlumoh <olumohbabatunde@gmail.com>
2025-11-01 09:36:52 +0000

    Enable soft delete for key vault secrets for consistency

--- Azure/Lab6/versions.tf
+++ Azure/Lab6/versions.tf
@@ -24,6 +24,9 @@ terraform {
 
 provider "azurerm" {
   features {
-    
+    key_vault {
+      purge_soft_deleted_secrets_on_destroy = true
+      recover_soft_deleted_secrets = true
+    }
   }
 }
