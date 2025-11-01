73f4e2357d79cd0527fc8db77314e81d53a4f4c2
BabaOlumoh <olumohbabatunde@gmail.com>
2025-11-01 09:01:56 +0000

    Create Entra ID Group and assign a user

---
+++ Azure/Lab6/iam.tf
@@ -0,0 +1,14 @@
+
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "remote_access_users" {
+  display_name     = "${var.application_name}-${var.environment_name}-remote_access_users"
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
+
+resource "azuread_group_member" "tunde_remote_access_membership" {
+  group_object_id  = azuread_group.remote_access_users.object_id
+  member_object_id = data.azuread_client_config.current.object_id
+}
--- Azure/Lab6/versions.tf
+++ Azure/Lab6/versions.tf
@@ -9,6 +9,10 @@ terraform {
     source = "hashicorp/random"
     version = "~> 3.6.3"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~>3.6.0"
+    }
     tls = {
       source = "hashicorp/tls"
       version = "4.1.0"
